3ae39ee77f6d05afadb825783f88a238693c704c
max <max.pavluschenko@gmail.com>
2018-07-11 15:17:37 +0300

    add project

---
+++ script/index.js
@@ -0,0 +1,9 @@
+menu.onclick = function hamburgerMenu() {
+    var x = document.getElementById('topnav');
+
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
